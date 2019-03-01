643ecf3a564290b0be84a99fa126cd714ba42326
daniel.pete@gmail.com <mittlillagithublogin>
2019-03-01 15:29:42 +0100

    DEVENV: Changed SpringBoot main class to Java. Only kotlin for dto's and entities

---
+++ web/src/main/java/se/inera/intyg/intygsbestallning/web/IntygsbestallningApplication.java
@@ -0,0 +1,13 @@
+package se.inera.intyg.intygsbestallning.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntygsbestallningApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(IntygsbestallningApplication.class, args);
+    }
+}
