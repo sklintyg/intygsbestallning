c7b52c732274ea542b45bfbd9a9bd78f53ff64cb
Magnus Ekstrand <magnus@ekstrand.net>
2019-03-08 10:37:23 +0100

    INTYG-8217: Tog bort FMU-relaterad kod

--- web/src/main/java/se/inera/intyg/intygsbestallning/web/auth/IbUser.java
+++ web/src/main/java/se/inera/intyg/intygsbestallning/web/auth/IbUser.java
@@ -38,11 +38,10 @@ import static se.inera.intyg.infra.security.authorities.AuthoritiesResolverUtil.
 import static se.inera.intyg.intygsbestallning.web.auth.authorities.AuthoritiesConstants.ROLE_BP_VARDADMIN;
 
 /**
- * The IB user overrides a lot of the default behaviour. Since users can be logged in on both Vårdgivare and on Vårdenhet
- * where the VG level doesn't require a Medarbetaruppdrag (IB uses system roles), some data structures are quite
+ * The IB user overrides a lot of the default behaviour. Since users can be logged
+ * in on both Vårdgivare and on Vårdenhet where the VG level doesn't require a
+ * Medarbetaruppdrag (IB uses system roles), some data structures are quite
  * unused while others have been added.
- *
- * @author eriklupander
  */
 public class IbUser extends IntygUser implements Serializable {
 
@@ -59,8 +58,6 @@ public class IbUser extends IntygUser implements Serializable {
     // Handles PDL logging state
     private Map<String, List<PdlActivityEntry>> storedActivities;
 
-    //private final IbRelayStateType relayState;
-
     // All known roles. Do NOT expose!!!
     @JsonIgnore
     private List<Role> possibleRoles;
@@ -70,7 +67,6 @@ public class IbUser extends IntygUser implements Serializable {
      */
     public IbUser(String hsaId, String namn) {
         super(hsaId);
-        //this.relayState = IbRelayStateType.FMU;
         this.storedActivities = new HashMap<>();
         this.hsaId = hsaId;
         this.namn = namn;
@@ -88,10 +84,8 @@ public class IbUser extends IntygUser implements Serializable {
      *            {@link org.springframework.security.saml.userdetails.SAMLUserDetailsService}
      *            implementor.
      */
-    //public IbUser(IntygUser intygUser, IbRelayStateType relayState) {
     public IbUser(IntygUser intygUser) {
         super(intygUser.getHsaId());
-        //this.relayState = relayState;
         this.personId = intygUser.getPersonId();
 
         this.namn = intygUser.getNamn();
@@ -110,73 +104,6 @@ public class IbUser extends IntygUser implements Serializable {
         this.storedActivities = new HashMap<>();
 
         this.miuNamnPerEnhetsId = intygUser.getMiuNamnPerEnhetsId();
-    }
-
-    public Map<String, List<PdlActivityEntry>> getStoredActivities() {
-        return storedActivities;
-    }
-
-    @Override
-    public int getTotaltAntalVardenheter() {
-        // count all hasid's in the datastructure
-        return (int) getVardgivare().stream().flatMap(vg -> vg.getHsaIds().stream()).count();
-    }
-
-
-    /**
-     * For IB, we select from the systemAuthorities tree rather than the traditional VG -> VE -> E tree.
-     *
-     * We also sets the currentRole based on the selection.
-     *
-     * @param vgOrVeHsaId
-     * @return
-     */
-    @Override
-    public boolean changeValdVardenhet(String vgOrVeHsaId) {
-        Map<String, Role> roleMap = new HashMap<>();
-        return changeValdVardenhetForBP(vgOrVeHsaId, roleMap);
-
-        // FOR FMU, we ignore the original HSA VG->VE tree, all authority is managed through the custom systemAuthorities tree.
-        /*
-        if (this.getRelayState() == IbRelayStateType.FMU) {
-            return changeValdVardenhetForFMU(vgOrVeHsaId, roleMap);
-        } else if (this.getRelayState() == IbRelayStateType.BP) {
-            return changeValdVardenhetForBP(vgOrVeHsaId, roleMap);
-        } else {
-            throw new IllegalStateException("Cannot change vardEnhet, user be in relayState FMU or BP");
-        }
-        */
-    }
-
-    private boolean changeValdVardenhetForBP(String vgOrVeHsaId, Map<String, Role> roleMap) {
-        for (IbVardgivare ibVg : systemAuthorities) {
-            for (IbVardenhet ibVardenhet : ibVg.getVardenheter()) {
-                if (ibVardenhet.getId().equalsIgnoreCase(vgOrVeHsaId)) {
-                    this.currentlyLoggedInAt = ibVardenhet;
-                    this.currentRole = selectRole(possibleRoles, ROLE_BP_VARDADMIN);
-                    roleMap.put(currentRole.getName(), currentRole);
-                    this.roles = roleMap;
-                    this.authorities = toMap(currentRole.getPrivileges(), Privilege::getName);
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-
-
-    private Role selectRole(List<Role> roles, String roleName) {
-        for (Role r : roles) {
-            if (r.getName().equals(roleName)) {
-                return r;
-            }
-        }
-        throw new IllegalStateException("Tried to set unknown role '" + roleName + "'");
-    }
-
-    @Override
-    public String getSelectedMedarbetarUppdragNamn() {
-        return null;
     }
 
     public List<IbVardgivare> getSystemAuthorities() {
@@ -203,11 +130,22 @@ public class IbUser extends IntygUser implements Serializable {
         this.currentlyLoggedInAt = currentlyLoggedInAt;
     }
 
+    public Map<String, List<PdlActivityEntry>> getStoredActivities() {
+        return storedActivities;
+    }
+
     public void setStoredActivities(Map<String, List<PdlActivityEntry>> storedActivities) {
         this.storedActivities = storedActivities;
     }
 
+    // Do not expose, only set method is allowed
+    public void setPossibleRoles(List<Role> possibleRoles) {
+        this.possibleRoles = possibleRoles;
+    }
+
+
     // Overridden stuff not used by IB
+
     @Override
     @JsonIgnore
     public SelectableVardenhet getValdVardgivare() {
@@ -238,6 +176,11 @@ public class IbUser extends IntygUser implements Serializable {
         return new ArrayList<>();
     }
 
+    @Override
+    public String getSelectedMedarbetarUppdragNamn() {
+        return null;
+    }
+
     /**
      * IB users are never lakare, override this for compatibility reasons.
      *
@@ -248,19 +191,48 @@ public class IbUser extends IntygUser implements Serializable {
         return false;
     }
 
-    // Do not expose.
-    public void setPossibleRoles(List<Role> possibleRoles) {
-        this.possibleRoles = possibleRoles;
+
+    @Override
+    public int getTotaltAntalVardenheter() {
+        // count all hasid's in the datastructure
+        return (int) getVardgivare().stream().flatMap(vg -> vg.getHsaIds().stream()).count();
     }
 
-    /*
-    @JsonIgnore
-    public IbRelayStateType getRelayState() {
-        return relayState;
+    /**
+     * For IB, we select from the systemAuthorities tree rather than the traditional VG -> VE -> E tree.
+     *
+     * We also sets the currentRole based on the selection.
+     *
+     * @param vgOrVeHsaId
+     * @return
+     */
+    @Override
+    public boolean changeValdVardenhet(String vgOrVeHsaId) {
+        for (IbVardgivare ibVg : systemAuthorities) {
+            for (IbVardenhet ibVardenhet : ibVg.getVardenheter()) {
+                if (ibVardenhet.getId().equalsIgnoreCase(vgOrVeHsaId)) {
+                    this.currentlyLoggedInAt = ibVardenhet;
+                    this.currentRole = selectRole(possibleRoles, ROLE_BP_VARDADMIN);
+                    this.authorities = toMap(currentRole.getPrivileges(), Privilege::getName);
+                    return true;
+                }
+            }
+        }
+        return false;
     }
-    */
+
 
     // private scope
+
+    private Role selectRole(List<Role> roles, String roleName) {
+        for (Role r : roles) {
+            if (r.getName().equals(roleName)) {
+                return r;
+            }
+        }
+        throw new IllegalStateException("Tried to set unknown role '" + roleName + "'");
+    }
+
     private void writeObject(java.io.ObjectOutputStream stream) throws java.io.IOException {
         stream.defaultWriteObject();
     }
