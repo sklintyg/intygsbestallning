b39be1a1fbcbcd7ed8ae6a57afa1737af74d3ce9
Martin Hesslund <martin.hesslund@callistaenterprise.se>
2019-03-11 11:47:04 +0100

    INTYG-8261: Lagt in normalize.css och styled components istället för material-ui

--- web/client/src/App.js
+++ web/client/src/App.js
@@ -1,21 +1,10 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 import {HashRouter, Switch, NavLink} from 'react-router-dom'
 import Route from 'react-router-dom/Route';
-import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
-import CssBaseline from '@material-ui/core/CssBaseline';
 import HomePage from "./pages/IndexPage";
 import ValjEnhetPage from "./pages/ValjEnhetPage";
 import BestallningarPage from "./pages/BestallningarPage";
 import BestallningPage from "./pages/BestallningPage";
-
-const theme = createMuiTheme({
-  typography: {
-    useNextVariants: true,
-    h3: {
-      fontSize: '2rem'
-    }
-  }
-});
 
 // TEST
 const TestLinks = () => (
@@ -34,16 +23,15 @@ const TestLinks = () => (
 const App = () => {
   return (
     <HashRouter>
-      <MuiThemeProvider theme={theme}>
-        <CssBaseline></CssBaseline>
-        <TestLinks />
-        <Switch>
-          <Route exact path="/" component={HomePage} />
-          <Route replace path="/valj-enhet" component={ValjEnhetPage} />
-          <Route replace path="/bestallningar" component={BestallningarPage} />
-          <Route path="/bestallning/:id" component={BestallningPage} />
-        </Switch>
-      </MuiThemeProvider>
+        <Fragment>
+          <TestLinks />
+          <Switch>
+            <Route exact path="/" component={HomePage} />
+            <Route replace path="/valj-enhet" component={ValjEnhetPage} />
+            <Route replace path="/bestallningar" component={BestallningarPage} />
+            <Route path="/bestallning/:id" component={BestallningPage} />
+          </Switch>
+        </Fragment>
     </HashRouter>
   )
 };
