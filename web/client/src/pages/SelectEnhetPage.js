888422650404a1478e8e647b40cdd36976285c6d
Marcus Cedergren <marcus.cedergren@callistaenterprise.se>
2019-03-25 10:18:42 +0100

    DEVENV: Välj enhet-sidan skall ha också ha footer enligt krav.

--- web/client/src/pages/SelectEnhetPage.js
+++ web/client/src/pages/SelectEnhetPage.js
@@ -1,18 +1,39 @@
 import React from 'react';
 import {FlexColumnContainer, ScrollingContainer, WorkareaContainer} from "../components/layout/body";
 import SelectEnhet from "../components/selectEnhet";
+import Footer from "../components/Footer/Footer";
+import styled from "styled-components";
+import ibValues from "../components/style/IbValues";
 
+const CustomScrollingContainer = styled(ScrollingContainer)`
+  max-width: none;
+`
+const PageContainer = styled(WorkareaContainer)`
+  margin: auto;
+  width: 100%;
+  max-width: ${ibValues.maxContentWidth};
+  display: flex;
+  flex-direction: column;
+  min-height: calc(100vh - 290px);
+  padding-bottom: 60px;
+  
+  img {
+  width: 100%;
+  padding-bottom: 20px;
+  }
+`
 const SelectEnhetPage = () => {
   return (
     <FlexColumnContainer>
-      <ScrollingContainer>
-        <WorkareaContainer>
+      <CustomScrollingContainer>
+        <PageContainer>
           <h1>Välj enhet</h1>
           <p>Du har behörighet för flera vårdenheter. Välj den du vill logga in på nu. Du kan byta enhet även efter
             inloggning.</p>
           <SelectEnhet />
-        </WorkareaContainer>
-      </ScrollingContainer>
+        </PageContainer>
+        <Footer />
+      </CustomScrollingContainer>
     </FlexColumnContainer>
   )
 };
