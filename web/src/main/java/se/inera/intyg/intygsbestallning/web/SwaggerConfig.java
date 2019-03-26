4914f07caf6b198756ecf010996ad5d5fd149a4a
Joakim Anell <jocke.anell@gmail.com>
2019-03-26 10:22:38 +0100

    DEVENV: katten paste hade varit i farten.

--- web/src/main/java/se/inera/intyg/intygsbestallning/web/SwaggerConfig.java
+++ web/src/main/java/se/inera/intyg/intygsbestallning/web/SwaggerConfig.java
@@ -3,11 +3,8 @@ package se.inera.intyg.intygsbestallning.web;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -23,16 +20,6 @@ public class SwaggerConfig {
                 .apis(RequestHandlerSelectors
                         .basePackage("se.inera.intyg.intygsbestallning.web.controller"))
                 .paths(PathSelectors.regex("/.*"))
-                .build().apiInfo(apiEndPointsInfo());
-    }
-    private ApiInfo apiEndPointsInfo() {
-        return new ApiInfoBuilder().title("Spring Boot REST API")
-                .description("Employee Management REST API")
-                .contact(new Contact("Ramesh Fadatare", "www.javaguides.net", "ramesh24fadatare@gmail.com"))
-                .license("Apache 2.0")
-                .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0.html")
-                .version("1.0.0")
                 .build();
     }
-
 }
