3f4dc3b4caae834aef1bb0def98a2db45bcc5d74
Joakim Anell <jocke.anell@gmail.com>
2019-05-02 16:12:24 +0200

    DEVENV: tog bort ickeanvända properties från buildSrc

--- buildSrc/src/main/kotlin/se/inera/intyg/intygsbestallning/build/Config.kt
+++ buildSrc/src/main/kotlin/se/inera/intyg/intygsbestallning/build/Config.kt
@@ -25,16 +25,11 @@ object Config {
     const val springBootVersion = "2.1.4.RELEASE"
     const val springDependencyManagementVersion = "1.0.7.RELEASE"
 
-    const val activemqVersion = "5.13.5"
     const val camelVersion = "2.23.1"
     const val camelBootStarterVersion = "2.23.1"
-    const val commonsLang3Version = "3.7"
     const val cxfBootStarterVersion = "3.3.0"
     const val guavaVersion = "27.1-jre"
     const val liquibaseVersion = "3.6.3"
-    const val jacksonVersion = "2.9.4"
-    const val javaxJmsVersion = "1.1-rev-1"
-    const val javaxMailVersion = "1.5.2"
     const val vavrVersion = "0.10.0"
     const val jaxServletApiVersion = "4.0.1"
     const val jaxVersion = "2.3.0"
@@ -45,7 +40,6 @@ object Config {
     const val swaggerVersion = "2.9.2"
     const val jakartaJwsVersion = "1.1.1"
     const val springSecuritySaml2Version = "1.0.3.RELEASE"
-
   }
 
   object TestDependencies {
@@ -53,8 +47,6 @@ object Config {
     const val junit4Version = "4.12"
     const val junit5Version = "5.4.1"
     const val awaitilityVersion = "1.4.0"
-    const val dbUnitVersion = "2.4.9"
-    const val springockitoVersion = "1.0.8"
     const val randomBeansVersion = "3.9.0"
     const val restAssuredVersion = "2.9.0"
     const val stAntlr4Version = "4.0.8"
