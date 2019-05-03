2ec96ae378492e4d3ab16eae1f0dabe0229d7d40
Marcus Cedergren <marcus.cedergren@callistaenterprise.se>
2019-05-03 11:18:53 +0200

    INTYGFV-11534: Fixat egna Legends till Piechart på welcomesidan

--- web/client/src/components/welcome/WelcomeChart.js
+++ web/client/src/components/welcome/WelcomeChart.js
@@ -1,23 +1,39 @@
 import React, { Fragment } from 'react'
 
 import ibColors from '../styles/IbColors'
-import { IbTypo02 } from '../styles/IbTypography'
+import { IbTypo02, IbTypo03 } from '../styles/IbTypography'
+import styled from 'styled-components'
 import ReactChartkick, { PieChart } from 'react-chartkick'
 import Chart from 'chart.js'
 
+const LegendBox = styled.span`
+  display: inline-block;
+  width: 14px;
+  height: 14px;
+  background-color: ${(props) => props.color};
+`
+
+const LegendText = styled(IbTypo03)`
+  display: inline-block;
+  padding-left: 8px;
+`
+const Legend = ({ color, text }) => (
+  <div>
+    <LegendBox color={color} />
+    <LegendText as="span">{text}</LegendText>
+  </div>
+)
 const customLabel = (tooltipItem, data) => {
   const dataValue = data.datasets[tooltipItem.datasetIndex].data[tooltipItem.index]
   const label = data.labels[tooltipItem.index].toLowerCase()
 
-  return dataValue + ' ' + label + ' beställningar'
+  return dataValue + ' ' + label + ' beställningar.'
 }
 
 ReactChartkick.addAdapter(Chart)
 let options = {
   legend: {
-    position: 'bottom',
-    onClick: null,
-    labels: { boxWidth: 14, fontSize: 18, fontFamily: 'Roboto', fontColor: ibColors.IB_COLOR_07 },
+    display: false,
   },
   tooltips: {
     backgroundColor: ibColors.IB_COLOR_17,
@@ -34,7 +50,8 @@ const WelcomeChart = (props) => (
   <Fragment>
     <IbTypo02 as="h1">Beställningar just nu</IbTypo02>
     <PieChart
-      width="300px"
+      width="200px"
+      height="220px"
       data={[
         ['Olästa', props.stats.antalOlastaBestallningar],
         ['Aktuella', props.stats.antalAktivaBestallningar],
@@ -43,6 +60,11 @@ const WelcomeChart = (props) => (
       colors={[ibColors.IB_COLOR_21, ibColors.IB_COLOR_06, ibColors.IB_COLOR_16]}
       library={options}
     />
+    <div>
+      <Legend color={ibColors.IB_COLOR_21} text="Olästa" />
+      <Legend color={ibColors.IB_COLOR_06} text="Aktuella" />
+      <Legend color={ibColors.IB_COLOR_16} text="Färdighanterade" />
+    </div>
   </Fragment>
 )
 
