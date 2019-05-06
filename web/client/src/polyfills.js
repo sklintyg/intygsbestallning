b5911c43540174e5cc7283baa9f2ab5a805f3ef4
Martin Hesslund <martin.hesslund@callistaenterprise.se>
2019-05-06 10:26:10 +0200

    DEVENV: Uppdaterat node deps

--- web/client/src/polyfills.js
+++ web/client/src/polyfills.js
@@ -1,4 +1,4 @@
 // Add imports from core-js as needed
-import 'react-app-polyfill/ie11';
-import 'core-js/fn/object/values'
-import 'core-js/fn/object/entries'
+import 'react-app-polyfill/ie11'
+import 'core-js/features/object/values'
+import 'core-js/features/object/entries'
